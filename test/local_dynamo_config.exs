ed92caf353a85d05f6cb4f34937d167046431dd7
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-08-26 16:52:25 -0500

    fix compilation error when ddbmodel is a dependency

--- test/local_dynamo_config.exs
+++ test/local_dynamo_config.exs
@@ -1,7 +1,7 @@
 require Record
 
 defmodule LocalDynamoConfig do
-  Record.defrecord :aws_config, Record.extract(:aws_config, from: "deps/erlcloud/include/erlcloud_aws.hrl")
+  Record.defrecord :aws_config, Record.extract(:aws_config, from_lib: "erlcloud/include/erlcloud_aws.hrl")
 
   def get do
     aws_config(ddb_scheme: 'http://', ddb_host: 'localhost', ddb_port: 8000, 
