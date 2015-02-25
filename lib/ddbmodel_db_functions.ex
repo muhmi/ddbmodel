97d4c5dfa98e90b461088dd6ef55d61f2086c67a
Roope Kangas <muhmi@iki.fi>
2015-02-25 13:56:18 +0200

    oops..

--- lib/ddbmodel_db_functions.ex
+++ lib/ddbmodel_db_functions.ex
@@ -1,14 +1,14 @@
 defmodule DDBModel.DB.Functions do
 
   def create_table(table_name, key, write_units, read_units) do
-    case DDBModel.DynamoDB.create_table(table_name, {key, :s}, key, write_units, read_units) do
+    case DDBModel.Database.create_table(table_name, {key, :s}, key, write_units, read_units) do
       {:ok, _result}   -> :ok
       error           -> error
     end
   end
 
   def delete_table(table_name) do
-    case DDBModel.DynamoDB.delete_table(table_name) do
+    case DDBModel.Database.delete_table(table_name) do
       {:ok, _result}   -> :ok
       error           -> error
     end
